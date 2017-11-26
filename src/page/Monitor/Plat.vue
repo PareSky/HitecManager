61f83485baf3c3e958123427dbeee137475a1fd8
swqh <swqh@swqhdeMacBook.local>
2017-11-26 18:41:20 +0800

    完善地图

--- src/page/Monitor/Plat.vue
+++ src/page/Monitor/Plat.vue
@@ -58,6 +58,9 @@
 <script>
 import AMap from 'AMap'
 import icon_1 from '@/assets/IconMedium/icon_car1.png'
+import icon_2 from '@/assets/IconMedium/icon_car2.png'
+import icon_3 from '@/assets/IconMedium/icon_car3.png'
+import icon_4 from '@/assets/IconMedium/icon_car4.png'
 var map
 export default {
   name: 'plat',
@@ -67,36 +70,72 @@ export default {
   data () {
     return {
       tableData: [
-      	{name:'武汉2号机',state:'正常',position:[121.533067,31.219374]},
-      	{name:'武汉2号机',state:'正常',position:[121.631946,31.211154]},
-      	{name:'武汉2号机',state:'正常',position:[121.733069,31.319376]},
-      	{name:'武汉2号机',state:'正常',position:[121.833088,31.219388]},
-      	{name:'武汉2号机',state:'正常',position:[121.933089,31.219389]},
-      ]
+      	{name:'武汉2号机',state:'正常',position:[121.533067,31.219374],icon:4},
+      	{name:'武汉2号机',state:'异常',position:[121.631946,31.211154],icon:3},
+      	{name:'武汉2号机',state:'故障',position:[121.733069,31.319376],icon:1},
+      	{name:'武汉2号机',state:'停机',position:[121.833088,31.219388],icon:2},
+      	{name:'武汉2号机',state:'正常',position:[121.933089,31.219389],icon:4},
+      ],
+      icons:[icon_1,icon_2,icon_3,icon_4],
+      markers:[],
+      map:{}
     }
   },
   methods: {
       init: function () {
-        var map = new AMap.Map('MapContainer',{
+        this.map = new AMap.Map('MapContainer',{
             resizeEnable: true,
             zoom: 10,
             center: [121.533069,31.219375]
         });
-        var icon1 = new AMap.Icon({
-        	image : icon_1,
-        	//size : new AMap.Size(24,24),
-        	imageSize: '100% 100%'
-				});
         var marker;
 				for(var i = 0; i < this.tableData.length; i += 1){
 					marker = new AMap.Marker({
-						icon : icon1,
+						icon : this.icons[this.tableData[i].icon-1],
 						position: this.tableData[i].position,
-						title: '2号机',
-						map: map
+						title: this.tableData[i].name,
+						map: this.map
 					});
+					marker.content=this.tableData[i];
+					this.markers.push(marker);
+					AMap.event.addListener(marker, 'click', this._onClick);
 				}
-      }
+				console.log(this.markers)
+      },
+     	_onClick : function(e){
+	   		console.log('_onClick')
+	   		var ct = e.target.content;
+	   		var title= ct.name;
+		    var info = [];
+		   info.push('编号：'+23898+'<br>');
+		   info.push('状态：'+ct.state+'<br>');
+		   info.push('位置：苏州市吴中区吴中大道 189号 <br>');
+		   var content = `
+		   	<div style='
+					background: #FFFFFF;
+					box-shadow: 0 0 4px 0 rgba(0,0,0,0.37);
+					border-radius: 6px;
+					text-align:left;
+					font-size: 14px;
+					padding:10px'>
+					<span style='
+						font-family: PingFangSC-Medium;
+						color: #107FE0;
+						letter-spacing: 0;'>${ct.name}</span><br>
+						编号：23894384<br>
+						状态：<span style='color:green'>${ct.state}</span><br>
+						位置：苏州市吴中区吴中大道 189号
+						</div>
+		   `;
+
+		   var infoWindow = new AMap.InfoWindow({
+				isCustom: true,  //使用自定义窗体
+				content: content,
+				offset: new AMap.Pixel(0, -50)//-113, -140
+		   });
+		   //infoWindow.setContent(e.target.content.state);
+		   infoWindow.open(this.map, e.target.getPosition());
+			}
     }
 }
 </script>
