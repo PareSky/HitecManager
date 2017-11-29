6d7ab7ccd0ec2b44e951d6e8b431f14c83482879
jp <617298698@qq.com>
2017-11-29 22:56:59 +0800

    time & bar

--- src/page/details/Living.vue
+++ src/page/details/Living.vue
@@ -3,13 +3,63 @@
     <div class='progressBox'>
     	<progress-column v-for="n in 8" :percentage='percentage' :unit='unit'/>
     </div>
-    <div class='mainItems'>mainItems</div>
+    <div class='mainItems'>
+      <div class='ItemBox'>
+        <img src='/src/assets/image/backgroundArrow.png'>
+        <div class='Items'>
+        <div>232</div>
+        <div class="hot">232</div>
+        <div>232</div>
+        <div>232</div>
+        <div>232</div>
+        <div>232</div>
+        <div>232</div>
+        <div>232</div>
+        <div class="cold">232</div>
+        </div>
+      </div>
+      <div class='ItemBox'>
+        <img src='/src/assets/image/backgroundArrow.png'>
+        <div class='Items'>
+        <div>232</div>
+        <div>232</div>
+        <div>232</div>
+        <div>232</div>
+        <div>232</div>
+        <div>232</div>
+        <div>232</div>
+        <div>232</div>
+        <div>232</div>
+        </div>
+      </div>
+    </div>
     <div class='progressBox Box2'>
     <progress-column v-for="n in 6" :percentage='percentage' :unit='unit'/>
     </div>
     <div class='UBox'>box1</div>
     <div class='UBox'>box2</div>
-    <div class='UBox'>box3</div>
+    <div class='UBox'>
+      <div class="TimeRow">
+        <div class='TimeImg'><img src='/src/assets/IconMedium/time1.png'></div>
+        <div class='TimeNum'>63.1</div>
+        <span>SEC</span>
+      </div>
+      <div class="TimeRow">
+        <div class='TimeImg'><img src='/src/assets/IconMedium/time2.png'></div>
+        <div class='TimeNum'>63.1</div>
+        <span>CNT</span>
+      </div>
+      <div class="TimeRow">
+        <div class='TimeImg'><img src='/src/assets/IconMedium/time3.png'></div>
+        <div class='TimeNum'>63.1</div>
+        <span>SEC</span>
+      </div>
+      <div class="TimeRow">
+        <div class='TimeImg'><img src='/src/assets/IconMedium/time4.png'></div>
+        <div class='TimeNum'>63.1</div>
+        <span>SEC</span>
+      </div>
+    </div>
 
 
   </div>
@@ -40,20 +90,69 @@ export default {
 	flex-flow: wrap ;
 }
 .progressBox{
-	width: 200px;
 	display: flex;
 	flex-flow: wrap;
+  width: 200px;
+  margin-left: 10px;
 }
 .Box2{
 	width: 180px;
 }
 .progressBox div{
 	margin-right: 20px;
+  margin-bottom: 10px;
 }
 .mainItems{
-	width: 50%;
+	width: 585px;
 }
 .UBox{
 	width: 33%;
+}
+.ItemBox{
+  position: relative;
+  margin-bottom: 25px;
+}
+.Items{
+  position: absolute;
+  top: -13px;
+  left: 33px;
+}
+.Items div{
+  float: left;
+  width: 36px;
+  height: 42px;
+  background: #f7931e;
+  border: 8px solid #e6e6e6;
+  line-height: 42px;
+  margin: 4px;
+  color: #fff;
+}
+div .hot{
+  background: #00a99d;
+}
+div .cold{
+  background: #b3b3b3;
+}
+.TimeRow{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  width: 231px;
+  margin-bottom: 10px;
+}
+.TimeRow .TimeImg{
+  width: 36px;
+}
+.TimeRow .TimeNum{
+  width: 115.34px;
+  height: 36.14px;
+  background: #e6e6e6;
+  font-size: 24px;
+  line-height: 36px;
+}
+.TimeRow span{
+  font-size: 24px;
+  color: #165f9b;
 }
 </style>
