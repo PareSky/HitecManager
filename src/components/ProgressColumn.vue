a5e760deab72be6ac194bf82884db632e9662a30
swqh <swqh@swqhdeMacBook.local>
2017-11-29 21:15:13 +0800

    living progress

---
+++ src/components/ProgressColumn.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="ProgressColumn">
+
+    <div class='ProBar'>
+    <div class='ProNum' :style="{height: percentage+'%'}"></div>
+    </div>
+    <span>{{percentage}}</span>
+    <span>{{unit}}</span>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProgressColumn',
+  props: ['percentage','unit'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.ProgressColumn{
+	display: flex;
+	flex-direction:column;
+	width:30px;
+}
+.ProBar{
+	position: relative;
+	height: 71px;
+	width: 26px;
+	background: #E6E6E6;
+}
+.ProNum{
+	position: absolute;
+	width: 100%;
+	background: #1D6CBB;
+	bottom: 0;
+}
+</style>
--- src/page/details/Living.vue
+++ src/page/details/Living.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="plat">
-    device
+  <div class="living">
+    <div class='progressBox'>
+    	<progress-column v-for="n in 8" :percentage='percentage' :unit='unit'/>
+    </div>
+    <div class='mainItems'>mainItems</div>
+    <div class='progressBox Box2'>
+    <progress-column v-for="n in 6" :percentage='percentage' :unit='unit'/>
+    </div>
+    <div class='UBox'>box1</div>
+    <div class='UBox'>box2</div>
+    <div class='UBox'>box3</div>
+
+
   </div>
 </template>
 
 <script>
+
+import ProgressColumn from '@/components/ProgressColumn'
 export default {
-  name: 'plat',
+	components:{
+		ProgressColumn
+	},
+  name: 'living',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      percentage: 89,
+      unit:'BAR'
     }
   }
 }
@@ -17,4 +34,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.living{
+	width: 1003.58px;
+	display: flex ;
+	flex-flow: wrap ;
+}
+.progressBox{
+	width: 200px;
+	display: flex;
+	flex-flow: wrap;
+}
+.Box2{
+	width: 180px;
+}
+.progressBox div{
+	margin-right: 20px;
+}
+.mainItems{
+	width: 50%;
+}
+.UBox{
+	width: 33%;
+}
 </style>
